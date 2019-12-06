2302928b8b7238444be86cfde4b5cfaff0514d1e
npushpakaran <nithinjith.pushpakaran@bolder.no>
2019-12-06 06:40:27 +0530

    JetPack Sample Initial Commit

---
+++ app/src/main/java/com/np/jetpacksample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.np.jetpacksample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
